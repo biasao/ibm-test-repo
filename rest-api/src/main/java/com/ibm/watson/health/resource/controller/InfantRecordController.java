3e5d2df31fa1bba7d186272f9eb84f1a4bb66597
Alberto Biasao <biasao@gmail.com>
2016-02-04 18:20:12 -0200

    fix import.

--- rest-api/src/main/java/com/ibm/watson/health/resource/controller/InfantRecordController.java
+++ rest-api/src/main/java/com/ibm/watson/health/resource/controller/InfantRecordController.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ibm.watson.health.entity.InfantRecord;
 import com.ibm.watson.health.service.InfantRecordService;
