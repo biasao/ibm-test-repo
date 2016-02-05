020f691c3438c8594d6a7702bd9ef5c3d20f1ec0
Alberto Biasao <biasao@gmail.com>
2016-02-04 23:43:41 -0200

    fix translate bean.

--- diagnostic/src/main/java/com/ibm/watson/health/service/bean/TranslateServiceBean.java
+++ diagnostic/src/main/java/com/ibm/watson/health/service/bean/TranslateServiceBean.java
@@ -1,9 +1,12 @@
 package com.ibm.watson.health.service.bean;
 
+import javax.inject.Named;
+
 import com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation;
 import com.ibm.watson.developer_cloud.language_translation.v2.model.TranslationResult;
 import com.ibm.watson.health.service.TranslateService;
 
+@Named
 public class TranslateServiceBean implements TranslateService {
 
 	private static String USER = "01d82b3c-078f-47a0-8d68-1eb0997c64ba";
